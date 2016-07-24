3d9da0eaf1fedf4726c6a6902af586de00af35b6
Nicholas J. Olson <nicholasjamesolson@gmail.com>
2016-07-23 20:27:49 -0500

    add bcrypt and update layout

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -5,3 +5,17 @@ get '/login' do
     erb :'/users/login'
   end
 end
+
+post '/login' do
+  if request.xhr?
+  else
+    @user = User.find_by(email: params[:email])
+    if @user && @user.save
+      session[:user_id]
+      redirect '/'
+    else
+      @errors = ['The most invalid credentials']
+      erb :'users/login'
+    end
+  end
+end
